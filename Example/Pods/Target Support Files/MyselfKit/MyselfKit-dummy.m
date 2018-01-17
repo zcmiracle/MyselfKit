94e7deb3cf112374da526d21c3f6b648029d6db3
Fearless <489368600@qq.com>
2018-01-17 10:28:48 +0800

    修改版本号0.1.0改为0.1.1

---
+++ Example/Pods/Target Support Files/MyselfKit/MyselfKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MyselfKit : NSObject
+@end
+@implementation PodsDummy_MyselfKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-MyselfKit_Example/Pods-MyselfKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyselfKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MyselfKit_Example
+@end
